f3db04697d6971a268f4794b8896c29f4b198d2e
mvdizel <mvdizel@users.noreply.github.com>
2015-09-05 11:57:35 -0400

    Initial Commit

---
+++ Four in Line/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Four in Line
+//
+//  Created by Василий Муравьев on 05.09.15.
+//  Copyright (c) 2015 Vasilii Muravev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Four in Line/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Four in Line
+//
+//  Created by Василий Муравьев on 05.09.15.
+//  Copyright (c) 2015 Vasilii Muravev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
