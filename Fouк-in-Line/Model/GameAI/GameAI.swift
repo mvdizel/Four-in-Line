b1ab70a3517ab8db0a90e2787fbb452d5f3e46d4
Unknown <mvdizel@gmail.com>
2017-12-23 21:40:30 -0500

    Game AI simplified and converted to Swift.

--- Fouк-in-Line/Model/GameAI/GameAI.swift
+++ Fouк-in-Line/Model/GameAI/GameAI.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+private let winScore: Float = 10.0
+private let maxDepth: Int = 3
+
 /// A singleton, defining Artificial Intelligence of game.
 final class GameAI {
   
@@ -15,8 +18,16 @@ final class GameAI {
   static let shared = GameAI()
   
   
+  // MARK: - Private Instance Attributes
+  var cells: [[Int]] = []
+  var madeMoves: [Int] = []
+  var bestColumn: Int!
+  
+  
   // MARK: - Initializers
-  private init() {}
+  private init() {
+    setup()
+  }
 }
 
 // MARK: Public Instance Methods
@@ -28,14 +39,53 @@ extension GameAI {
   ///   - gameBoard: A `GameBoard` with current game state.
   ///   - completion: A handler, which fires, when AI found the best move.
   func bestMove(on gameBoard: GameBoard, completion: @escaping (ChipPosition?) -> Void) {
-    let testPosition = firstAvailableMove(on: gameBoard)
-    completion(testPosition)
+//    let testPosition = firstAvailableMove(on: gameBoard)
+//    completion(testPosition)
+//    return
+    cells = []
+    gameBoard.cells.forEach { rows in
+      var column: [Int] = []
+      rows.forEach({ player in
+        if let cellType = player?.rawValue {
+          column.append(cellType)
+          return
+        }
+        let cellType = (column.last ?? 0) < 2 ? 2 : 3
+        column.append(cellType)
+      })
+      column.append(3)
+      cells.append(column)
+    }
+    madeMoves = cells.map { column in
+      return column.filter({ $0 < 2 }).count
+    }
+    let alpha = -Float.infinity
+    let beta = Float.infinity
+    var iteration = 0
+    var depth = 0
+    bestColumn = nil
+    _ = minimax(
+      for: .phone,
+      alpha: alpha,
+      beta: beta,
+      iteration: &iteration,
+      depth: &depth
+    )
+    let position = ChipPosition(
+      column: bestColumn,
+      row: madeMoves[bestColumn],
+      player: .phone
+    )
+    completion(position)
   }
 }
 
 
 // MARK: Private Instance Methods
 private extension GameAI {
+  
+  func setup() {
+  }
   
   /// Searches for the first available move on the gameboard.
   ///
@@ -49,5 +99,51 @@ private extension GameAI {
       return ChipPosition(column: $0, row: row, player: .phone)
     }
     return positions.first
+  }
+  
+  func minimax(for player: Player, alpha: Float, beta: Float, iteration: inout Int, depth: inout Int) -> Float {
+    var alpha = alpha
+    depth += 1
+    defer {
+      depth -= 1
+    }
+    // @TODO: Check if winner found on current move.
+    // @TODO: Update score with current depth.
+    // @TODO: Check for forks.
+    iteration += 1
+    for column in 0..<DynamicConstants.numberOfColumns.value-1 {
+      let row = madeMoves[column]
+      guard row < DynamicConstants.numberOfRows.value else {
+        continue
+      }
+      cells[column][row] = player.rawValue
+      cells[column][row + 1] = 2
+      madeMoves[column] += 1
+      var score = positionScore(for: player)
+      if score < winScore, depth < maxDepth {
+        score = minimax(
+          for: player.next(),
+          alpha: -beta,
+          beta: -alpha,
+          iteration: &iteration,
+          depth: &depth
+        )
+      }
+      cells[column][row] = 2
+      cells[column][row + 1] = 3
+      madeMoves[column] -= 1
+      if score > alpha {
+        alpha = score
+        bestColumn = column
+      }
+      if alpha >= beta {
+        break
+      }
+    }
+    return -alpha
+  }
+  
+  func positionScore(for player: Player) -> Float {
+    return 10
   }
 }
